5a6db62933df380c149ca152c45620df6b9ecf35
Drew Niman <drewmniman@gmail.com>
2024-11-13 13:18:28 -0800

    Finish repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
+  let res = "";
+  for (let i = 0; i < times; i++) {
+    res += string;
+  }
+  return res;
 };
 
 // Do not edit below this line
